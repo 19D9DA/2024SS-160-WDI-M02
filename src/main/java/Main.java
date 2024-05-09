89c9a980f35b623431457329b7a8298529341ebb
Johannes Regneri <45144658+JohannesRegneri@users.noreply.github.com>
2024-05-10 01:08:44 +0200

    new Car in Main Java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,8 @@ public class Main {
         Car car1 = new Car("red", "BMW", 5);
         Car car2 = new Car("green", "Audi", 3);
         Car car3 = new Car("blue", "VW", 3);
-
         Car car4 = new Car("yellow", "Renault", 4);
+		Car car5 = new Car("white", "Smart", 4);
 
         //An dieser Stelle befindet sich nun ein Array
         //Dieses Array wird mit einem Sortieralgorithmus sortiert
